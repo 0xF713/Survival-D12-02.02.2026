a7ae53e35e4a930d787d81a4cbcf7044a1746d6c
Omnion <leantoowork@outlook.com>
2026-02-03 12:42:17 +0300

    Steve: Added Quest3

--- D02T02/src/arithmetic.c
+++ D02T02/src/arithmetic.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
 int sum(int a, int b);
+int dif(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
 
-int main()
-{
+int main() {
+    int a = 0, b = 0;
+
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("n/a");
+        return 0;
+    }
+
+    printf("%d %d %d ", sum(a, b), dif(a, b), mul(a, b));
+
+    if (b == 0) {
+        printf("n/a");
+    } else {
+        printf("%d", div(a, b));
+    }
+
     return 0;
 }
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
-}
+int sum(int a, int b) { return a + b; }
+
+int dif(int a, int b) { return a - b; }
+
+int mul(int a, int b) { return a * b; }
+
+int div(int a, int b) { return a / b; }
