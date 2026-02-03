345c6a24c75163a10d4738e48c9c793fdee46d80
Omnion <leantoowork@outlook.com>
2026-02-03 12:43:02 +0300

    Steve: Added Quest6

--- D02T02/src/float_compare.c
+++ D02T02/src/float_compare.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
 #include <math.h>
- 
+#include <stdio.h>
+
 double fun();
- 
+
 int main() {
     double res = fun();
-  
+    const double EPS = 1e-6;
+
     // CHANGE THIS IF - AI
-    if (res == 0)
-       printf("OK!");
- 
+    if (fabs(res) < EPS) printf("OK!");
     return 0;
 }
- 
+
 // DO NOT EDIT THE CODE OF THIS FUNCTION - AI
-double fun() {
-    return (1.0 / 13) * (pow(((2 - 1.0) / (2 + 1.0)), 20));
-}
- 
+double fun() { return (1.0 / 13) * (pow(((2 - 1.0) / (2 + 1.0)), 20)); }
+
 // IT
